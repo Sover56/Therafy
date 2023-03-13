7b1858f482442325abe2f0ac84e6bcbc59afa529
Harris434 <2019puulay@gmail.com>
2023-03-13 15:56:04 +0630

    resources update

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
-const content = document.getElementById("content")
-const home = `<div w3-include-html="./home/home.html"></div>`;
-const about = `<div w3-include-html="./about/about.html"></div>`
-const resources = `<div w3-include-html="./resources/resources.html"></div>`
-const blog = `<div w3-include-html="./blog/blog.html"></div>`
-const meet = `<div w3-include-html="./meet/meet.html"></div>`
+const content = document.getElementById("content");
+const home = `<div w3-include-html="./pages/home.html"></div>`;
+const about = `<div w3-include-html="./pages/about.html"></div>`;
+const resources = `<div w3-include-html="./pages/resources.html"></div>`;
+const blog = `<div w3-include-html="./pages/blog.html"></div>`;
+const meet = `<div w3-include-html="./pages/meet.html"></div>`;
 
 const routes = {
-  404 : '404.html', 
-  '/index.html' : './home/home.html',
-  '/' : './home/home.html',
-  '/about' : './about/about.html',
-  '/resource' : './resources/resources.html',
-  '/blog' : './blog/blog.html',
-  '/meet' : './meet/meet.html'
+  404: "404.html",
+  "/index.html": "./pages/home.html",
+  "/": "./pages/home.html",
+  "/about": "./pages/about.html",
+  "/resources": "./pages/resources.html",
+  "/blog": "./pages/blog.html",
+  "/meet": "./pages/meet.html",
 };
 
 const route = (event) => {
   event = event || window.event;
   event.preventDefault();
-  window.history.pushState(
-    {},
-    "",
-    event.target.href
-  )
+  window.history.pushState({}, "", event.target.href);
   handleLocation();
-}
+  console.log(event.target.href);
+};
 
 const handleLocation = async () => {
   const path = window.location.pathname;
-  const route = routes[path] || routes[404]
+  const route = routes[path] || routes[404];
   const html = await fetch(route).then((data) => data.text());
   content.innerHTML = html;
-}
+};
 
 window.onpopstate = handleLocation;
 window.route = route;
