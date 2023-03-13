d9c85465a7af27d36229ec1c85827bbdd2a3c18f
Sover56 <swamyeeh56@gmail.com>
2023-03-14 00:44:27 +0630

    hamburger classList Remove update

--- hamburger.js
+++ hamburger.js
@@ -5,3 +5,9 @@ hamburger.addEventListener("click", ()=> {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active")
 })
+
+document.querySelectorAll(".home-btn, .about-btn, .blog-btn, .resource-btn, .meet-btn").forEach(n => n.
+  addEventListener("click", ()=>{
+  hamburger.classList.remove("active")
+  navMenu.classList.remove("active")
+}))
