47665648a933be00f94a180b904cdab1d2c49653
Sover56 <swamyeeh56@gmail.com>
2023-03-13 14:01:00 +0630

    update

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /* Loop through a collection of all HTML elements: */
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      /* Make an HTTP request using the attribute value as the file name: */
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /* Remove the attribute, and call this function once more: */
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
-        }
-      }
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /* Exit the function: */
-      return;
-    }
-  }
-}
-
 const content = document.getElementById("content")
 const home = `<div w3-include-html="./home/home.html"></div>`;
 const about = `<div w3-include-html="./about/about.html"></div>`
@@ -34,23 +6,34 @@ const blog = `<div w3-include-html="./blog/blog.html"></div>`
 const meet = `<div w3-include-html="./meet/meet.html"></div>`
 
 const routes = {
-  '/' : home,
-  '/about' : about,
-  '/resources' : resources,
-  '/blog' : blog,
-  '/meet' : meet
+  404 : '404.html', 
+  '/index.html' : './home/home.html',
+  '/' : './home/home.html',
+  '/about' : './about/about.html',
+  '/resource' : './resources/resources.html',
+  '/blog' : './blog/blog.html',
+  '/meet' : './meet/meet.html'
 };
 
-const onNavigate = (pathname) => {
+const route = (event) => {
+  event = event || window.event;
+  event.preventDefault();
   window.history.pushState(
     {},
-    pathname,
-    window.location.origin + pathname
+    "",
+    event.target.href
   )
-  content.innerHTML = routes[pathname]
-  window.onpopstate()
+  handleLocation();
 }
 
-window.onpopstate = () => {
-  content.innerHTML = routes[window.location.pathname];
+const handleLocation = async () => {
+  const path = window.location.pathname;
+  const route = routes[path] || routes[404]
+  const html = await fetch(route).then((data) => data.text());
+  content.innerHTML = html;
 }
+
+window.onpopstate = handleLocation;
+window.route = route;
+
+handleLocation();
