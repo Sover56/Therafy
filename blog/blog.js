a331d6d1778bea92eece2d1444718b1d8a1086b6
Sover56 <swamyeeh56@gmail.com>
2023-03-14 07:19:06 +0630

    buttons update

--- blog/blog.js
+++ blog/blog.js
@@ -6,10 +6,10 @@ const commentSection = document.querySelector(".comment-section")
 commentSection.style.display = "none"
 
 const fetchPosts = async () => {
- const res = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XFd7B4nKLEBQFFHi06Eb/comments?item_id=post')
- let posts = await res.json()
- let i = 1
- posts.forEach((response) => {
+  const res = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XFd7B4nKLEBQFFHi06Eb/comments?item_id=post')
+  let posts = await res.json()
+  let i = 1
+  posts.forEach((response) => {
     mainSection.innerHTML += `
       <div class="post" id="post-${i}" >
         <div class="poster">
@@ -21,22 +21,22 @@ const fetchPosts = async () => {
         </div>
         <p class="inst-text"> Double click to view </p>
       </div>
-    ` 
+    `
     i++;
   })
   let commentList = document.querySelector(".comment-list")
-  const postList = document.querySelectorAll(".post")  
-postList.forEach(post => { 
-  post.addEventListener("click",() => 
-  { console.log(post.id)
-    showComments(post.id)
-    const commentBtn = document.getElementById("comment-btn")
-    if (!commentBtn) {
-      commentList.innerHTML += 
-      `<button id="comment-btn" onclick="postComment('${post.id}')"> Submit </button>`
-    } 
-   } )
-});
+  const postList = document.querySelectorAll(".post")
+  postList.forEach(post => {
+    post.addEventListener("click", () => {
+      console.log(post.id)
+      showComments(post.id)
+      const commentBtn = document.getElementById("comment-btn")
+      if (!commentBtn) {
+        commentList.innerHTML +=
+          `<button id="comment-btn" onclick="postComment('${post.id}')"> Submit </button>`
+      }
+    })
+  });
 
 
 }
@@ -51,7 +51,7 @@ function displayPopup() {
   popup.style.display = "block"
   posterInput.value = ""
   postInput.value = ""
-  
+
 }
 
 
@@ -59,34 +59,34 @@ function displayPopup() {
 // POST FUNCTION //
 
 
-function post() { 
+function post() {
   var x;
   let postInput = document.getElementById("post-input")
   let posterInput = document.getElementById("poster-input")
   let mainSection = document.getElementById("main-section")
 
   const header = {
-      accept: "application.json",
-      "content-type": "application/json",
+    accept: "application.json",
+    "content-type": "application/json",
   };
   const data = {
-      item_id: "post",
-      username: posterInput.value,
-      comment: postInput.value,
+    item_id: "post",
+    username: posterInput.value,
+    comment: postInput.value,
   };
   const options = {
-      method: "POST",
-      headers: header,
-      body: JSON.stringify(data),
+    method: "POST",
+    headers: header,
+    body: JSON.stringify(data),
   };
-  
-  
+
+
   fetch(
-      "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XFd7B4nKLEBQFFHi06Eb/comments/",
-      options
+    "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XFd7B4nKLEBQFFHi06Eb/comments/",
+    options
   ).then((response) => console.log(response))
 
-  
+
   mainSection.innerHTML += `<div class="post" id="'post'" onclick="showComments()"->
   <div class="poster">
     <img class="pfp" src="assets/pfp.jpg">
@@ -95,80 +95,80 @@ function post() {
   <div class="post-content">
     <p id="post-text"> ${postInput.value} </p>
   </div>
-</div>` 
-x++
+</div>`
+  x++
   posterInput.value = ""
   postInput.value = ""
   popup.style.display = "none"
 
-  }
+}
 
 
 function showComments(postId) {
   const commentText = document.getElementById("comment-text")
   const commentorName = document.getElementById("commentor-name")
   commentSection.style.display = "block"
-  
+
   fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XFd7B4nKLEBQFFHi06Eb/comments?item_id=${postId}`)
-  .then(response => response.json())
-  .then(response=> {
-    commentText.innerHTML = ""
-    commentorName.innerHTML = ""
-    response.forEach((username)=> {  
-      console.log(commentText)
+    .then(response => response.json())
+    .then(response => {
+      commentText.innerHTML = ""
+      commentorName.innerHTML = ""
+      response.forEach((username) => {
+        console.log(commentText)
         commentText.innerHTML += `<li> ${username.commentBtn}</li>`
         commentorName.innerHTML += `<li> ${username.username} </li>`
 
+      })
     })
-  })
-  .catch(error => {
-    commentText.innerHTML = "No Comments"
-    commentorName.innerHTML = "There are"
-  })
+    .catch(error => {
+      commentText.innerHTML = "No Comments"
+      commentorName.innerHTML = "There are"
+    })
 }
 
 
-  //comment function //
+//comment function //
 
 
- function postComment (postId) {
+function postComment(postId) {
   let usernameInput = document.getElementById("username-input")
   let commentInput = document.getElementById("comment-input")
-const commentorName = document.getElementById("commentor-name")
-const commentText = document.getElementById("comment-text")
+  const commentorName = document.getElementById("commentor-name")
+  const commentText = document.getElementById("comment-text")
 
   const header = {
-      accept: "application.json",
-      "content-type": "application/json",
+    accept: "application.json",
+    "content-type": "application/json",
   };
   const data = {
-      item_id:  postId,
-      username: usernameInput.value,
-      comment: commentInput.value,
+    item_id: postId,
+    username: usernameInput.value,
+    comment: commentInput.value,
   };
   const options = {
-      method: "POST",
-      headers: header,
-      body: JSON.stringify(data),
-  }; 
-fetch(
-      "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XFd7B4nKLEBQFFHi06Eb/comments?item_id=comment",
-      options
-  ) .then((response) => console.log(response))
-  .then(response => {
-    commentText.innerHTML +=`<li>
-    ${commentInput.value} </li> ` 
+    method: "POST",
+    headers: header,
+    body: JSON.stringify(data),
+  };
+  fetch(
+    "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XFd7B4nKLEBQFFHi06Eb/comments?item_id=comment",
+    options
+  ).then((response) => console.log(response))
+    .then(response => {
+      commentText.innerHTML += `<li>
+    ${commentInput.value} </li> `
       commentorName.innerHTML += `<li>
       ${usernameInput.value} </li>`
-      if ( usernameInput.value =="") {
+      if (usernameInput.value == "") {
         commentorName.innerHTML += ` <li> Anonymous </li>`
       } else {
-        usernameInput.value  = usernameInput.value
+        usernameInput.value = usernameInput.value
         commentInput.value = commentInput.value
       }
       commentInput.value = ""
       usernameInput.value = ""
-  })
+    })
 
 }
 
